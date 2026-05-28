05c19a923b1678a56101b73d02b54d70be6e1145
Wesley-Programers <twesley967@gmail.com>
2026-05-28 19:44:26 -0300

    refactor(service): migrate and refine ChangeName logic
    -Clean up method signatures to match the new Service structure. -Implement proper error wrapping using fmt.Errorf for better traceability. -Finalized the split of the service

--- internal/service/change_name.go
+++ internal/service/change_name.go
@@ -1,13 +1,37 @@
 package service
 
-import "ShieldAuth-API/internal/repository"
+import (
+	"fmt"
+	"context"
+	
+	"ShieldAuth-API/internal/repository"
+)
 
 type ChangeNameService struct {
-	repo repository.ChangeName
+	repo repository.ChangeNameStruct
 }
 
-func NewChangeNameService(repo repository.ChangeName) *ChangeNameService {
+func NewChangeNameService(repo repository.ChangeNameStruct) *ChangeNameService {
 	return &ChangeNameService{
 		repo: repo,
 	}
+}
+
+
+func (changeName *ChangeNameService) ChangeNameFunction(ctx context.Context, input ChangeNameInput) error {
+
+	user, err := changeName.repo.GetForChangeName(ctx, input.ID)
+	if err != nil {
+		return fmt.Errorf("getting user by id: %w", err)
+	}
+
+	if err := user.ChangeName(input.CurrentName, input.NewName); err != nil {
+		return fmt.Errorf("changing user name: %w", err)
+	}
+
+	if err := changeName.repo.UpdateName(ctx, user); err != nil {
+		return fmt.Errorf("changing user name: %w", err)
+	}
+
+	return nil
 }
