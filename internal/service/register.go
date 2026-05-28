a2ac04d34b23730b602acf038a42a34aaa756f3f
Wesley-Programers <twesley967@gmail.com>
2026-05-28 19:40:22 -0300

    refactor(service): migrate and enhance register logic to RegisterService
    Add better error wraping for registration failured. -Finalized the split of the service

--- internal/service/register.go
+++ internal/service/register.go
@@ -1,17 +1,54 @@
 package service
 
 import (
+	"context"
+	"fmt"
+
+	"ShieldAuth-API/internal/domain"
 	"ShieldAuth-API/internal/repository"
 	"ShieldAuth-API/internal/security"
+	"ShieldAuth-API/internal/security/argon2"
+	"ShieldAuth-API/internal/security/hibp"
 )
 
 type RegisterService struct {
-	repo repository.User
-	argon security.Argon2Hasher
+	repo repository.UserRepository
+	hasher argon2.Argon2Hasher
+	hibp hibp.HIBPChecker
+	security security.PasswordLeakChecker
 }
 
-func NewRegisterService(repo repository.User) *RegisterService {
+func NewRegisterService(repo repository.UserRepository, hibp hibp.HIBPChecker, hasher argon2.Argon2Hasher) *RegisterService {
 	return &RegisterService{
 		repo: repo,
+		hibp: hibp,
+		hasher: hasher,
 	}
+}
+
+
+func (register *RegisterService) RegisterFunction(ctx context.Context, input RegisterInput) error {
+	if err := security.VerifyPassword(register.security, input.Password); err != nil {
+		return err
+	}
+
+	hash, err := register.hasher.Hash(input.Password)
+	if err != nil {
+		return err
+	}
+
+	defer security.ZeroMemory(input.Password)
+
+	user := &domain.User{
+		Name: input.Name,
+		Email: input.Email,
+		PasswordHash: []byte(hash),
+	}
+
+	err = register.repo.Create(ctx, user)
+	if err != nil {
+		return fmt.Errorf("failed to register user: %w", err)
+	}
+
+	return nil
 }
