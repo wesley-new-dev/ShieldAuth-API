e31cd3ea717dc3bd46b4d3d8533a6d40eca5668c
Wesley-Programers <twesley967@gmail.com>
2026-05-28 19:35:59 -0300

    refactor(service): migrate and enhance login logic to LoginService
    -Add content timeout and memory zeroing for the password. -Finalized the split of the service

--- internal/service/login.go
+++ internal/service/login.go
@@ -1,13 +1,62 @@
 package service
 
-import "ShieldAuth-API/internal/repository"
+import (
+	"context"
+	"time"
 
-type LoginService struct {
-	repo repository.LoginUser
+	"ShieldAuth-API/internal/domain"
+	"ShieldAuth-API/internal/security"
+	"ShieldAuth-API/internal/security/argon2"
+)
+
+var dummyArgon2Hash = []byte("DPunA5HgdFJkHrryZptqsQLwmAB4NfaRoM/TiI3Elg01fD2iGX7DuMlQ6B6KATx")
+
+type LoginRepository interface {
+	GetByIdentifier(ctx context.Context, identifier string) (*domain.User, error)
 }
 
-func NewLoginService(repo repository.LoginUser) *LoginService {
+type LoginService struct {
+	repo LoginRepository
+	hasher argon2.Argon2Hasher
+}
+
+func NewLoginService(repo LoginRepository, hasher argon2.Argon2Hasher) *LoginService {
 	return &LoginService{
 		repo: repo,
+		hasher: hasher,
 	}
+}
+
+
+func (s *LoginService) VerifyLoginFunction(ctx context.Context, input LoginInput) (int, error) {
+
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+	identifier := input.Email
+	if identifier == "" {
+		identifier = input.Name
+	}
+
+	if identifier == "" {
+		return 0, domain.ErrInvalidData
+	}
+
+	defer security.ZeroMemory(input.Password)
+
+	user, err := s.repo.GetByIdentifier(ctx, identifier)
+	if err != nil {
+		_ = s.hasher.Compare(input.Password, dummyArgon2Hash)
+		return 0, domain.ErrInvalidCredentials
+	}
+
+	if err := ctx.Err(); err != nil {
+		return 0, err
+	}
+
+	if err := s.hasher.Compare(input.Password, user.PasswordHash); err != nil {
+		return 0, domain.ErrInvalidCredentials
+	}
+
+	return user.Id, nil
 }
