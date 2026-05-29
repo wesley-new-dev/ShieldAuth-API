72d0738ac09c6837312cd891e05ed0eaa6708739
Wesley-Programers <twesley967@gmail.com>
2026-05-29 19:32:36 -0300

    refactor(security): implement secure type for credentials and blind ZeroMemory

--- internal/security/memory.go
+++ internal/security/memory.go
@@ -1,11 +1,39 @@
 package security
 
-import "runtime"
+import (
+	"bytes"
+	"runtime"
+	"unsafe"
+)
+
+type SecretBytes []byte
+
+func (s *SecretBytes) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 || bytes.Equal(b, []byte("null")) {
+		return nil
+	}
+
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		b = b[1 : len(b)-1]
+	}
+
+	dst := make([]byte, len(b))
+	copy(dst, b)
+
+	*s = dst
+	return nil
+}
 
 func ZeroMemory(b []byte) {
+	if len(b) == 0 {
+		return
+	}
+
 	for i := range b {
 		b[i] = 0
 	}
+
+	_ = *(*byte)(unsafe.Pointer(&b[0]))
 
 	runtime.KeepAlive(b)
 }
