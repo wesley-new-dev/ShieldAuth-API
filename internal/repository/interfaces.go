165f058ed13ca2770cad0e5049837fa8aeb418dc
Wesley-Programers <twesley967@gmail.com>
2026-06-14 15:57:24 -0300

    feat(repository): add RefreshTokenRepository interface

--- internal/repository/interfaces.go
+++ internal/repository/interfaces.go
@@ -7,11 +7,23 @@ import (
 )
 
 type UserRepository interface {
-	Create(ctx context.Context, user *domain.User) error
+	Create(ctx context.Context, user *domain.User) (int64, error)
 
 	GetByID(ctx context.Context, id int64) (*domain.User, error)
 	GetByEmail(ctx context.Context, email string) (*domain.User, error)
 
 	Update(ctx context.Context, user *domain.User) error
-	Delete(ctx context.Context, id int64) error
+	Delete(ctx context.Context, id int) error
+}
+
+type RefreshTokenRepository interface {
+	FindByHash(ctx context.Context, hash []byte) (*RefreshTokenRepository, error)
+
+	Revoke(ctx context.Context, token_hash []byte) error
+	RevokeSession(ctx context.Context, sessionID string) error
+	RevokeAllUserSession(ctx context.Context, userID int64) error
+
+	UpdateLastUsed(ctx context.Context, id int64) error
+
+	Create(ctx context.Context, token *RefreshTokenRepository) error
 }
