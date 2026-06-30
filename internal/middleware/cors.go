917b446ece57a0eca27ded516b287c450b308775
Wesley-Programers <twesley967@gmail.com>
2026-06-30 17:28:11 -0300

    chore(middleware): move hardcoded CORS origin to environment variable

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,11 +2,18 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 )
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+
+		allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+		if allowedOrigin == "" {
+			return
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
