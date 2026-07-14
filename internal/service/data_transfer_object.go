57398514852987885637fba2ca92e54466adc218
Wesley-Programers <twesley967@gmail.com>
2026-07-14 18:09:06 -0300

    refactor(service): secure password field using SensitiveData in service data transfer object

--- internal/service/data_transfer_object.go
+++ internal/service/data_transfer_object.go
@@ -1,51 +1,59 @@
 package service
 
+import "ShieldAuth-API/internal/security"
+
 type RegisterInput struct {
-	Name     	string
-	Email    	string
-	Password 	[]byte
+	Name     string
+	Email    string
+	Password *security.SensitiveData
 }
 
 type LoginInput struct {
-	Name     	string
-	Email    	string
-	Password 	[]byte
+	Name     string
+	Email    string
+	Password *security.SensitiveData
 }
 
 type ResetPasswordInput struct {
 	Token           string
-	NewPassword     []byte
-	ConfirmPassword []byte
+	NewPassword     *security.SensitiveData
+	ConfirmPassword *security.SensitiveData
 }
 
 type ChangeNameInput struct {
-	ID             	int
-	CurrentName    	string
-	NewName        	string
-	ConfirmNewName 	string
+	ID             int
+	CurrentName    string
+	NewName        string
+	ConfirmNewName string
 }
 
 type ChangeEmailInput struct {
-	ID             	int
+	ID              int
 	CurrentEmail    string
 	NewEmail        string
 	ConfirmNewEmail string
-	Password        []byte
+	Password        *security.SensitiveData
 }
 
 type ChangePasswordInput struct {
-	UserID 				int
-	CurrentPassword 	[]byte
-	NewPassword 		[]byte
-	ConfirmPassword 	[]byte
+	UserID          int
+	CurrentPassword *security.SensitiveData
+	NewPassword     *security.SensitiveData
+	ConfirmPassword *security.SensitiveData
 }
 
 type DeleteAccountInput struct {
-	UserID 				int
-	CurrentPassword 	[]byte
+	UserID          int
+	CurrentPassword *security.SensitiveData
 }
 
 type LogOutInput struct {
-	UserID 				int
-	CurrentPassword 	[]byte
+	RefreshToken  []byte
+	AccessTokenID string
+	UserID        int64
+	SessionID     string
+}
+
+type RefreshSessionInput struct {
+	OldRefreshToken string
 }
