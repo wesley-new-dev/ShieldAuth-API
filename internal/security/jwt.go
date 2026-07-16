d0c1d4ba428d3e7a4f295cdd22ccd2748d92da98
Wesley-Programers <twesley967@gmail.com>
2026-07-16 20:43:26 -0300

    feat(security): include session ID and token version in JWT claims

--- internal/security/jwt.go
+++ internal/security/jwt.go
@@ -7,15 +7,20 @@ import (
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+
+	"ShieldAuth-API/internal/middleware"
 )
 
-
-func TokenJWT(userID int64) (string, error) {
-	claims := jwt.RegisteredClaims{
-		Subject: strconv.FormatInt(userID, 10),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(15 * time.Minute)),
-		IssuedAt: jwt.NewNumericDate(time.Now()),
-		ID: uuid.NewString(),
+func TokenJWT(userID int64, jwtVersion int, sessionID string) (string, error) {
+	claims := middleware.Claims{
+		Version:   jwtVersion,
+		SessionID: sessionID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   strconv.FormatInt(userID, 10),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(15 * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ID:        uuid.NewString(),
+		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
