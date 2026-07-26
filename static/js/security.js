d95fd940bd6413b124ecbd6da8affd6882947da8
Wesley-Programers <twesley967@gmail.com>
2026-07-26 18:56:41 -0300

    feat(static): add submit handler for change password form

--- static/js/security.js
+++ static/js/security.js
@@ -1,3 +1,5 @@
+import { fetchWithAuth } from "./api";
+
 let inputCurrentEmail = document.getElementById("currentEmail");
 let inputNewEmail = document.getElementById("newEmail");
 let inputConfirmNewEmail = document.getElementById("confirmNewEmail");
@@ -9,14 +11,17 @@ let theSameName = document.getElementById("newNameIsTheSame");
 let mainEmail = document.getElementById("main-email");
 let mainName = document.getElementById("main-name");
 
-let changeEmail = document.getElementById("change-email");
 let changeName = document.getElementById("change-name");
+let changeEmail = document.getElementById("change-email");
+let changePassword = document.getElementById("change-password");
 
 let change = document.getElementById("div-change-name-window");
 let changeEmailWindow = document.getElementById("div-change-email-window");
+let changePasswordWindow = document.getElementById("div-change-password-window");
 
 let leaveChangeName = document.getElementById("leaveChangeName");
 let leaveChangeEmail = document.getElementById("leaveChangeEmail");
+let leaveChangePassword = document.getElementById("leaveChangePassword");
 let leaveDeleteAccount = document.getElementById("leaveDeleteAccount");
 
 let deleteAccountForm = document.getElementById("deleteAccountForm");
@@ -30,41 +35,29 @@ let someErrorDeleteAccount = document.getElementById("someErrorDeleteAccount");
 changeName.addEventListener("click", () => {
     change.style.display = "block";
 });
-
-
-leaveDeleteAccount.addEventListener("click", () => {
-    deleteSession.style.display = 'none';
-
-    inputPasswordDeleteAccount.value = '';
-    inputEmailDeleteAccount.value = '';
+changePassword.addEventListener("click", () => {
+    changePasswordWindow.style.display = 'block';
 });
-
-
-leaveChangeName.addEventListener("click", () => {
-
-    let inputCurrentName = document.getElementById("currentName");
-    let inputNewName = document.getElementById("newName");
-
-    inputCurrentName.value = '';
-    inputNewName.value = '';
-
-    change.style.display = 'none';
-});
-
-
 changeEmail.addEventListener("click", () => {
     changeEmailWindow.style.display = 'block';
 });
 
+
+leaveDeleteAccount.addEventListener("click", () => {
+    deleteSession.style.display = 'none';
+    inputPasswordDeleteAccount.value = '';
+    inputEmailDeleteAccount.value = '';
+});
+leaveChangeName.addEventListener("click", () => {
+    change.style.display = 'none';
+});
 leaveChangeEmail.addEventListener("click", () => {
-
-    inputCurrentEmail.value = '';
-    inputNewEmail.value = '';
-    inputConfirmNewEmail.value = '';
-    inputCurrentPassword.value = '';
-
     changeEmailWindow.style.display = 'none';
 });
+leaveChangePassword.addEventListener("click", () => {
+    changePasswordWindow.style.display = 'none';
+});
+
 
 deleteAccount.addEventListener("click", () => {
     deleteSession.style.display = 'block';
@@ -73,7 +66,10 @@ deleteAccount.addEventListener("click", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    let formEmail = document.getElementById("formChangeEmail");
+    let formChangeEmail = document.getElementById("formChangeEmail");
+    let formChangeName = document.getElementById("formChangeName");
+    let formChangePassword = document.getElementById("formChangePassword");
+
     let currentNameWrongMessage = document.getElementById("currentNameWrongMessage");
 
     let differentEmailsMessage = document.getElementById("diferentEmailsMessage");
@@ -82,56 +78,32 @@ document.addEventListener("DOMContentLoaded", () => {
     let theSameEmail = document.getElementById("newEmailIsTheSame");
     let currentEmailWrong = document.getElementById("currentEmailWrongMessage");
 
-    let form = document.getElementById("formChangeName");
     let someErrorMessage = document.getElementById("someErrorMessage");
 
-    form.addEventListener("submit", async (e) => {
+    formChangeName.addEventListener("submit", async (form) => {
+
         let button = document.getElementById("saveNewName");
-
-        e.preventDefault();
-        const tokenKey = localStorage.getItem("jwt_key");
-
-        if (!tokenKey) {
-            alert("No token found. Please log in.");
-            return;
-        }
+        form.preventDefault();
 
         button.disabled = true;
         const formData = new FormData(e.target);
 
         try {
 
-            const fetchChangeName = await fetch("http://127.0.0.1:8000/change", {
+            const response = await fetchWithAuth("http://127.0.0.1:8000/change/name", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `Bearer ${tokenKey}`
                 },
                 body: JSON.stringify(Object.fromEntries(formData.entries())),
             })
 
-            const status = fetchChangeName.status
-            const message = await fetchChangeName.text()
-            alert(`STATUS: ${status} MESSAGE: ${message}`);
-
-            if (status === 200 && message === "success") {
-
-                alert("EVERYTHING IS WORK");
+            if (response.ok) {
                 window.location.reload();
-
-            } else if (status === 400 && message === "INVALID NAME") {
-                currentNameWrongMessage.style.display = 'block';
-                
-            } else if (status === 400 && message === "NAMES IS NOT DIFFERENT") {
-                theSameName.style.display = 'block';
-
-            } else {
-                someErrorMessage.style.display = 'block';
-            };
+            }
 
         } catch ( error ) {
-            console.error("ERROR: ", error)
-            alert("SOME ERROR");
+            console.error("error: ", error)
 
         } finally {
             button.disabled = false;
@@ -140,49 +112,27 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-    formEmail.addEventListener("submit", async (a) => {
+    formChangeEmail.addEventListener("submit", async (form) => {
 
-        const tokenKey = localStorage.getItem("jwt_key");
         let button = document.getElementById("saveNewEmail");
 
-        a.preventDefault();
+        form.preventDefault();
         button.disabled = true;
         const formDataEmail = new FormData(a.target);
 
         try {
-            const fetchChangeEmail = await fetch("http://127.0.0.1:8000/email", {
+            const response = await fetchWithAuth("http://127.0.0.1:8000/change/email", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `Bearer ${tokenKey}`
                 },
                 credentials: "include",
                 body: JSON.stringify(Object.fromEntries(formDataEmail.entries())),
             });
 
-            const status = fetchChangeEmail.status
-            const message = await fetchChangeEmail.text()
-            alert(`STATUS: ${status} MESSAGE: ${message}`);
-
-            if (status === 200) {
-                alert("EVERYTHING OK");
+            if (response.ok) {
                 window.location.reload();
-
-            } else if (status === 400 && message === "EMAIL IS NOT THE SAME") {
-                currentEmailWrong.style.display = 'block';
-
-            } else if (status === 400 && message === "NEW EMAIL WRONG") {
-                differentEmailsMessage.style.display = 'block';
-
-            } else if (status === 400 && message === "INCORRECT PASSWORD") {
-                incorrectPasswordMessage.style.display = 'block';
-
-            } else if (status === 400 && message === "EMAIL IS NOT DIFFERENT") {
-                theSameEmail.style.display = 'block';
-
-            } else {
-                someErrorEmailMessage.style.display = 'block';
-            };
+            }
 
         } catch ( error ) {
             console.error("ERROR: ", error)
@@ -198,43 +148,60 @@ document.addEventListener("DOMContentLoaded", () => {
         let button = document.getElementById("buttonDeleteAccount");
 
         form.preventDefault();
+
         button.disabled = true;
         const formData = new FormData(form.target);
 
         try {
             
-            const deleteAccountFetch = await fetch("http://127.0.0.1:8000/delete", {
+            const response = await fetchWithAuth("http://127.0.0.1:8000/delete", {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify(Object.fromEntries(formData.entries())),
             });
 
-            const status = deleteAccountFetch.status
-            const message = await deleteAccountFetch.text()
-            alert(`Status: ${status} Message: ${message}`);
-
-            if (status === 200 && message === "success") {
-
-            } else if (status === 400 && message === "INCORRECT PASSWORD") {
-                incorrectPasswordDeleteAccount.style.display = 'block';
-
-            } else if (status === 400 && message === "INCORRECT EMAIL") {
-                incorrectEmailDeleteAccount.style.display = 'block';
-
-            } else {
-                someErrorDeleteAccount.style.display = 'block';
-            };
+            if (response.ok) {
+                window.location.href = '../html/login.html'
+            }
 
         } catch (error) {
             console.error("ERROR: ", error);
-            alert("SOME ERROR");
 
         } finally {
             button.disabled = false;
         };
         
     });
+
+    formChangePassword.addEventListener("submit", async (form) => {
+
+        let button = document.getElementById("saveNewPassword");
+        form.preventDefault();
+
+        button.disabled = true;
+        const newFormData = new FormData(form.target);
+        
+        try {
+
+            const response = await fetchWithAuth("http://127.0.0.1:8000/change/password", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(Object.fromEntries(newFormData.entries())),
+            })
+
+            if (response.ok) {
+                window.location.reload();
+            };
+
+        } catch(error) {
+            console.error("error: ", error)
+        } finally {
+            button.disabled = false;
+        }
+    })
 
 });
