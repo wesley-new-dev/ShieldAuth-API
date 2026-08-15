b7ce81ec111b1cfc1d4d821fee5768f656cc3f02
Wesley-Programers <twesley967@gmail.com>
2026-08-15 18:18:03 -0300

    feat(service/auth): implement reset code verification and single-use token generation

--- internal/service/auth/valid_token.go
+++ internal/service/auth/valid_token.go
@@ -2,33 +2,53 @@ package auth
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"ShieldAuth-API/internal/domain"
+	"ShieldAuth-API/internal/security"
 	"ShieldAuth-API/internal/security/redis"
 )
 
-
 type ValidTokenService struct {
 	resetStore redis.PasswordResetStore
+	tokenManager security.TokenManager
 }
 
-
-func NewValidToken(resetStore redis.PasswordResetStore) *ValidTokenService {
+func NewValidToken(resetStore redis.PasswordResetStore, tokenManager security.TokenManager) *ValidTokenService {
 	return &ValidTokenService{
 		resetStore: resetStore,
+		tokenManager: tokenManager,
 	}
 }
 
-
-func (v *ValidTokenService) ValidToken(ctx context.Context, token string) error {
-	if token == "" {
-		return domain.ErrInvalidToken
+func (v *ValidTokenService) ValidToken(ctx context.Context, code string) (string, error) {
+	if code == "" {
+		return "", domain.ErrInvalidToken
 	}
 
-	_, err := v.resetStore.Get(ctx, token)
+	saveCodeKey := fmt.Sprintf("reset-password-code:%s", code)
+	if status, err := v.resetStore.Exists(ctx, saveCodeKey); err != nil {
+		return "", domain.ErrInternal
+	} else if !status {
+		return "", domain.ErrInvalidToken
+	}
+
+	if err := v.resetStore.Delete(ctx, saveCodeKey); err != nil {
+		return "", domain.ErrInternal
+	}
+
+	token, err := v.tokenManager.GenerateToken()
 	if err != nil {
-		return domain.ErrInvalidToken
+		return "", domain.ErrInternal
 	}
 
-	return nil
+	tokenHash := v.tokenManager.TokenHash(token)
+
+	key := fmt.Sprintf("reset-password-token:%s", tokenHash)
+	if err := v.resetStore.Save(ctx, key, tokenHash, 5*time.Minute); err != nil {
+		return "", domain.ErrInternal
+	}
+
+	return token, nil
 }
